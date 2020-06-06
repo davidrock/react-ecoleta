158b51aa20edd979af8d38a39e3faa805d0e3f33
David Rock <david.martinelli@picpay.com>
2020-06-06 16:02:42 -0300

    Create post route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,15 @@
 import express from 'express';
 import routes from './routes';
+import path from 'path';
 
 const app = express();
+app.use('/uploads', express.static(path.resolve(__dirname, 'uploads')));
+// // parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }))
+
+// // parse application/json
+// app.use(bodyParser.json())
+app.use(express.json());
 app.use(routes);
 
 
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,57 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-	response.json({ mg: 'Hello World' });
+// routes.get('/', (request, response) => {
+// 	response.json({ mg: 'Hello World' });
+// });
+
+routes.get('/items', async (request, response) => {
+	const items = await knex('items').select('*');
+
+	items.map(
+		(x) => (x.image = `http://localhost:3333/uploads/${x.image}`)
+	);
+
+	response.json(items);
+});
+
+routes.post('/points', async (request, response) => {
+	const trx = await knex.transaction();
+	const {
+		name,
+		email,
+		whatsapp,
+		latitude,
+		longitude,
+		city,
+		uf,
+		items,
+	} = request.body;
+
+	const insertedIds = await trx('points').insert({
+		name,
+		email,
+		image: 'image-fale',
+		whatsapp,
+		latitude,
+		longitude,
+		city,
+		uf
+		// items,
+	});
+
+	const pointItems = items.map((item_id: number)=> {
+		return {
+			item_id,
+			point_id: insertedIds[0]
+		}
+	});
+
+	await trx(`point_items`).insert(pointItems);
+
+	return response.json({ success: true });
 });
 
 export default routes;
